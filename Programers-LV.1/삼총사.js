65b74831a0ed85a6fa60868a0e43b5d79f32eac5
yunsoo1 <shin4895@gmail.com>
2023-02-20 00:43:19 +0900

    Update 삼총사.js

--- Programers-LV.1/삼총사.js
+++ Programers-LV.1/삼총사.js
@@ -1,24 +1,29 @@
 function solution(number) {
-    var answer = 0;
-    for(let i=0; i<number.length; i++){
-        for(let j=1; j<number.length; j++){
+    let answer = 0;
+    
+    for (let i = 0; i < number.length; i++) {
+        for (let j = i + 1; j < number.length; j++) {
             let num=0;
             num=number[i]+number[j];
             const 더한값이외요소 = [...number];
             더한값이외요소.splice(i,1);
             더한값이외요소.splice(j-1,1);
             console.log(더한값이외요소);
-            memoization(num)
-            
-        }
 
+    return answer;
+  }
+}
+
+  
+  function memoization(더한값, 더한값이외요소) {
+    let count = 0;
+    for (let i = 0; i < 더한값이외요소.length; i++) {
+      if (더한값이외요소[i] === 더한값) {
+        count++;
+      }
     }
-    
+    return count;
+  }
 }
-solution([-2, 3, 0, 2, -5]);
-
-
-function memoization(더한값){
-    더한값
-
-}
+  
+  solution([-2, 3, 0, 2, -5]);
