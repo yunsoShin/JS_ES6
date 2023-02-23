e4cdad57f932424c7a3e926c9d29ebd9482998a7
yunsoo1 <shin4895@gmail.com>
2023-02-23 23:29:44 +0900

    Create 개인정보수집.js

---
+++ Programers-LV.1/개인정보수집.js
@@ -0,0 +1,53 @@
+function solution(today, terms, privacies) {
+    var answer = [];
+    const result = {};
+    
+    
+
+    for (const item of privacies) {
+        const [key, value] = item.split(' ');
+        result[value] = key;
+    }
+
+
+    console.log(terms[1].replace(/\s+/g, '').split(''));
+    for(let i=0; i<terms.length; i++){
+        
+        const term = terms[i].replace(/\s+/g, '').split('').slice(1).join('');
+        
+        console.log(term);
+    }
+    
+    console.log(terms);
+
+    
+    return terms;
+}
+
+function solution(today, terms, privacies) {
+    var answer = [];
+    const result = {};
+    
+    
+
+    for (const item of privacies) {
+        const [key, value] = item.split(' ');
+        result[value] = key;
+    }
+    const onlynumPrivacies =privacies.map((element) => element.replace(/\D/g, ""));
+    const onlynumToday = today.replace(/\D/g, "");
+    const onlyAlphabetPrivacies = privacies.map(str => str.match(/[A-Z]/)[0]);
+    
+    const term = terms.map(a=>a.replace(/\s+/g, ''))
+    
+    for(let i=0; i<terms.length; i++){
+        const num = term[i].split('').slice(1).join
+        
+    }
+    
+    
+    
+    
+    
+    return term;
+}
