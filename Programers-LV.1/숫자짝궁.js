cf99c72ba6a8c23eaf678b4ff298c043a09a8893
yunsoo1 <shin4895@gmail.com>
2023-02-28 22:49:57 +0900

    Update 숫자짝궁.js

--- Programers-LV.1/숫자짝궁.js
+++ Programers-LV.1/숫자짝궁.js
@@ -28,3 +28,33 @@ function findDuplicateCharacters(str) {
 
   console.log(findDuplicateCharacters("1231414256")); 
   console.log(findDuplicates('1231415678899')); 
+
+  function solution(X, Y) {
+    function countDigits(str) {
+  		const counts = new Array(10).fill(0); // 0에서 9까지 숫자마다의 개수를 저장할 배열을 만듭니다.
+  		for (let i = 0; i < str.length; i++) {
+    		const digit = parseInt(str[i]);
+    
+      		counts[digit]++;
+    
+  		}
+  		return counts;
+	}
+
+    let answer=[]
+    
+    const countX = countDigits(X);
+    const countY = countDigits(Y);
+    const big = Math.max(X.length,Y.length)
+    console.log(big)
+    for(let i=9; i>=0; i--){ 
+        if(countX[i]>0&&countY[i]>0){
+            
+            answer.push
+            
+        }
+        console.log(i,countX[i],countY[i])
+    }
+    console.log(countX,countY)
+    return answer
+}
