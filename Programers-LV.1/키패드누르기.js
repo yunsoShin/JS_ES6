05e138d1e660ebd31823f9f36f1ddf97133c3fa2
yunsoo1 <shin4895@gmail.com>
2023-03-01 21:24:25 +0900

    Update 키패드누르기.js

--- Programers-LV.1/키패드누르기.js
+++ Programers-LV.1/키패드누르기.js
@@ -62,3 +62,31 @@ function solution(numbers, hand) {
     console.log(result,Rlocate,Llocate)
     return result.join('');
 }
+
+function solution(numbers, hand) {
+    hand = hand[0] === "r" ? "R" : "L"
+    let position = [1, 4, 4, 4, 3, 3, 3, 2, 2, 2]
+    let h = { L: [1, 1], R: [1, 1] }
+    return numbers.map(x => {
+      if (/[147]/.test(x)) {
+        h.L = [position[x], 1]
+        return "L"
+      }
+      if (/[369]/.test(x)) {
+        h.R = [position[x], 1]
+        return "R"
+      }
+      let distL = Math.abs(position[x] - h.L[0]) + h.L[1]
+      let distR = Math.abs(position[x] - h.R[0]) + h.R[1]
+      if (distL === distR) {
+        h[hand] = [position[x], 0]
+        return hand
+      }
+      if (distL < distR) {
+        h.L = [position[x], 0]
+        return "L"
+      }
+      h.R = [position[x], 0]
+      return "R"
+    }).join("")
+  }
