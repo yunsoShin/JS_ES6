99d9f5526b05ee464a2503b18be8017ae7b6db25
yunsoo1 <shin4895@gmail.com>
2023-03-06 19:04:26 +0900

    Create 올바른괄호.js

---
+++ Programmers-Lv.2/올바른괄호.js
@@ -0,0 +1,15 @@
+function solution(s) {
+    const stack = [];
+    for (let i = 0; i < s.length; i++) {
+      if (s[i] === "(") {
+        stack.push(s[i]);
+      } else if (s[i] === ")") {
+        if (stack.length === 0) {
+          return false;
+        } else {
+          stack.pop();
+        }
+      }
+    }
+    return stack.length === 0;
+  }
