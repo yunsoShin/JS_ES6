e950549e2610cc3e76fc0829af94cb1a09e3b6b0
yunsoo1 <shin4895@gmail.com>
2023-03-07 20:54:40 +0900

    Update 파보나치.js

--- Programmers-Lv.2/파보나치.js
+++ Programmers-Lv.2/파보나치.js
@@ -17,3 +17,27 @@ function solution(n) {
     
     return answer ;
 }
+
+function solution(n) {
+    let answer = 0;
+    
+    function fibonacci(n, memo = {}) {
+        if (n in memo) {
+            return memo[n];
+        }
+        if (n <= 1) {
+            return n;
+        }
+  
+        memo[n] = (fibonacci(n-1, memo) + fibonacci(n-2, memo))%1234567;
+        return memo[n];
+    }
+
+    
+    answer = fibonacci(n)
+    
+    
+    
+    
+    return answer ;
+}
