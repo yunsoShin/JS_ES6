34b6f50165b2e751ae857bb6af7bf8e7f51f8798
yunsoo1 <shin4895@gmail.com>
2023-03-09 23:53:10 +0900

    Create 짝지어제거하기.js

---
+++ Programmers-Lv.2/짝지어제거하기.js
@@ -0,0 +1,13 @@
+function solution(s){
+    if (s.length % 2 !== 0) return 0;
+    let stack = [s[0]];
+    for (let i = 1; i < s.length; i+=1){
+        stack.push(s[i]);
+        if (stack.slice(-2, -1)[0] === s[i]){
+            stack.pop()
+            stack.pop()
+        }
+    }
+    if (stack.length > 0) return 0;
+    return 1;
+}
