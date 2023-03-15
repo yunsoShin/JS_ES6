025ab8f7636e643ae2d9e5cb35bdcb5d5c2ce776
yunsoo1 <shin4895@gmail.com>
2023-03-15 23:00:47 +0900

    Create 귤고르기.js

---
+++ Programmers-Lv.2/귤고르기.js
@@ -0,0 +1,17 @@
+function solution(k, tangerine) {
+    const answer = [];
+    let result = 0;
+    let count =0;
+    const uniqueArray = new Set(tangerine);
+    for (const value of uniqueArray) {
+        answer.push(tangerine.filter(a=>a===value).length)
+        console.log(answer)
+        
+    }
+    answer.sort((a,b)=>a-b)
+    while(result<k){
+        result+=answer.pop();
+        count++
+    }
+    return count;
+}
