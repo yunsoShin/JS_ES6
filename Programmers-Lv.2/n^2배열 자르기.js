86793e46a1cccf71426afc110be74332d99bfa34
yunsoo1 <shin4895@gmail.com>
2023-03-17 23:32:38 +0900

    Update n^2배열 자르기.js

--- Programmers-Lv.2/n^2배열 자르기.js
+++ Programmers-Lv.2/n^2배열 자르기.js
@@ -1,4 +1,13 @@
-export function solution(n, left, right) {
-    // ...
+function solution(n, left, right) {
+    const result = [];
+    for (let i = left; i <= right; i++) {
+      const row = Math.floor(i / n);
+      const col = i % n;
+      const val = Math.max(row, col) + 1;
+      result.push(val);
+    }
+    return result;
   }
+  
+  console.log(solution(4, 7, 14)); // [4, 3, 3, 3, 4, 4, 4, 4]
   
