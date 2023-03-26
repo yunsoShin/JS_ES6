c6ecfa31e0049f8e6ce732b8866c62d7382db048
yunsoo1 <shin4895@gmail.com>
2023-03-26 23:53:17 +0900

    Create 가장많이등장하는문자찾기.js

---
+++ Programmers-Lv.2/가장많이등장하는문자찾기.js
@@ -0,0 +1,23 @@
+function findMostFrequentWord(text) {
+    const wordHash = {};
+    let maxCount = 0;
+    let mostFrequentWord = '';
+  
+    const words = text.split(' ');
+  
+    for (const word of words) {
+      wordHash[word] = (wordHash[word] || 0) + 1;
+  
+      if (wordHash[word] > maxCount) {
+        maxCount = wordHash[word];
+        mostFrequentWord = word;
+      }
+    }
+  
+    return mostFrequentWord;
+  }
+  
+  const inputText = 'the quick brown fox jumps over the lazy dog and the quick brown dog jumps again';
+  const frequentWord = findMostFrequentWord(inputText);
+  console.log(frequentWord); // 출력: 'the'
+  
