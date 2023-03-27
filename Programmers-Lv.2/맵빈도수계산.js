840342f8c6354c10baeedaaf5381323ac65ec005
yunsoo1 <shin4895@gmail.com>
2023-03-27 23:51:02 +0900

    Create 맵빈도수계산.js

---
+++ Programmers-Lv.2/맵빈도수계산.js
@@ -0,0 +1,15 @@
+function countFrequencies(arr) {
+    const frequencyMap = new Map();
+  
+    for (const element of arr) {
+      const count = frequencyMap.get(element) || 0;
+      frequencyMap.set(element, count + 1);
+    }
+  
+    return frequencyMap;
+  }
+  
+  const numbers = [1, 2, 3, 2, 1, 3, 4, 1, 2, 3, 5];
+  const frequencies = countFrequencies(numbers);
+  console.log([...frequencies]); // 출력: [ [1, 3], [2, 3], [3, 3], [4, 1], [5, 1] ]
+  
