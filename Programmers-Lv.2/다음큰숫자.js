e181afc7cbed2dea05f799c72cab925ebc38642f
Shin <shin4895@gmail.com>
2023-08-09 21:41:12 +0900

    Create 다음큰숫자.js

---
+++ Programmers-Lv.2/다음큰숫자.js
@@ -0,0 +1,8 @@
+function solution(n) {
+  var 엔의일의개수 = n.toString(2).split("1").length; // 뉴진수로 변환한 n
+  let answer = n;
+  while (true) {
+    answer++;
+    if (엔의일의개수 === answer.toString(2).split("1").length) return answer;
+  }
+}
