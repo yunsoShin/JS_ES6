c29081479643dff54ee37fbb086dfe1b38941fa5
Shin <shin4895@gmail.com>
2023-08-14 23:05:07 +0900

    Update 소수만들기.js

--- 소수만들기.js
+++ 소수만들기.js
@@ -1,27 +1,27 @@
 function solution(nums) {
-    let count = 0;
-    function isPrime(number) {
-        if (number < 2) {
-            return false;
-        }
+  let count = 0;
+  function isPrime(number) {
+    if (number < 2) {
+      return false;
+    }
 
     for (let i = 2; i <= Math.sqrt(number); i++) {
-        if (number % i === 0) {
-            return count;
-        }
+      if (number % i === 0) {
+        return count;
+      }
     }
-
-        return count++;
+    //sss
+    return count++;
+  }
+  let answer = 0;
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        answer = nums[i] + nums[j] + nums[k];
+        isPrime(answer);
+      }
     }
-    let answer = 0;
-    nums.sort((a,b)=>a-b);
-    for(let i=0; i<nums.length; i++){
-        for(let j=i+1; j<nums.length; j++){
-            for(let k=j+1; k<nums.length; k++){
-                answer=nums[i]+nums[j]+nums[k]
-                isPrime(answer)
-            }
-        }
-    }
-    return count;
+  }
+  return count;
 }
