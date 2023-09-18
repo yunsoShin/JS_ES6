cb9b7a3efe5dc160b703f4b6ab60708f213c0c78
Shin <shin4895@gmail.com>
2023-09-18 22:53:56 +0900

    Update 괄호검사.js

--- 괄호검사.js
+++ 괄호검사.js
@@ -1,16 +1,16 @@
 function isValidParentheses(s) {
-    const stack = [];
-    const map = { '(': ')', '[': ']', '{': '}' };
-    
-    for (let i = 0; i < s.length; i++) {
-      if (map[s[i]]) {
-        // 열린 괄호인 경우, 스택에 추가합니다.
-        stack.push(s[i]);
-      } else if (s[i] !== map[stack.pop()]) {
-        // 닫힌 괄호인 경우, 스택에서 열린 괄호를 꺼내 짝을 검사합니다.
-        return false;
-      }
+  const stack = [];
+  const map = { "(": ")", "[": "]", "{": "}" };
+
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]]) {
+      // 열린 괄호인 경우, 스택에 추가합니다.
+      stack.push(s[i]);
+    } else if (s[i] !== map[stack.pop()]) {
+      // 닫힌 괄호인 경우, 스택에서 열린 괄호를 꺼내 짝을 검사합니다.ㄴㄴㄴ
+      return false;
     }
-    
-    return stack.length === 0; // 스택이 비어있는지 확인합니다.
   }
+
+  return stack.length === 0; // 스택이 비어있는지 확인합니다.
+}
