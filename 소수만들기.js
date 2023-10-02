acf4a973f3daede54813abe52071bbd214e81a92
Shin <shin4895@gmail.com>
2023-10-02 23:13:08 +0900

    Update 소수만들기.js

--- 소수만들기.js
+++ 소수만들기.js
@@ -10,11 +10,12 @@ function solution(nums) {
         return count;
       }
     }
+    addEventListener;6094
     //sssggff
     return count++;
   }
   let answer = 0;
-  nums.sort((a, b) => a - b);
+  nums.sort((a, b) => a - b);∑
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       for (let k = j + 1; k < nums.length; k++) {
