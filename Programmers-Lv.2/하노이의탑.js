00881c03a3420e78fcfbea244e1c33dc6cd16801
shin yun soo <91814941+yunsoShin@users.noreply.github.com>
2023-12-12 21:12:50 +0900

    Create 하노이의탑.js

---
+++ Programmers-Lv.2/하노이의탑.js
@@ -0,0 +1,1 @@
+function solution(n) { let answer = []; function hanoi(n, from, to, via) { if(n === 1) answer.push([from, to]); else { hanoi(n - 1, from, via, to); answer.push([from, to]); hanoi(n - 1, via, to, from); } } hanoi(n,1,3,2); return answer; }
